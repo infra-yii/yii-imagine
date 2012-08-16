7e4981a598dee9a41dadc8491dc3d2aeec41d06a
alari <name.alari@gmail.com>
2012-08-16 11:46:54 +0400

    basic implementation

---
+++ ImagineYii.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author alari
+ * @since 8/16/12 11:38 AM
+ */
+class ImagineYii extends CComponent
+{
+    public $driver = "gd2";
+
+    /**
+     * @return Imagine\Image\ImageInterface
+     */
+    public function getImagine() {
+        Yii::setPathOfAlias("Imagine", __DIR__."/Imagine/lib/Imagine");
+        switch($this->driver) {
+            case "gd2": return new Imagine\Gd\Imagine();
+            case "imagick": return new Imagine\Imagick\Imagine();
+            case "gmagick": return new Imagine\Gmagick\Imagine();
+        }
+    }
+}
